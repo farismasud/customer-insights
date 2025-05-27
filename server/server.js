42300cd44512d313611610a830ffc4277f3261a9
farismasud <masudfaris54@gmail.com>
2025-05-27 19:13:07 +0700

    update depedencies

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -7,14 +8,14 @@ const customerRoutes = require('./routes/customerRoutes');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/customerDB', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
